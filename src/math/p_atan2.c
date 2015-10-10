4a7a7b9b883c781d346252ef9bceb3d2e8de40df
Tom Anderson <twocs@users.noreply.github.com>
2015-10-10 17:34:40 +1030

    include code for _p_atan

--- src/math/p_atan2.c
+++ src/math/p_atan2.c
@@ -3,6 +3,25 @@
 
 static const float pi_2 = (float) M_PI * 0.5f;
 
+
+/*
+ * code from pal/p_atan()
+ * -1 <= x <= 1
+ * atan x = a1 * x + a3 * x^3 + ... + a9 * x^9 + e(x)
+ * |e(x)| <= 10^-5
+ */
+static inline float _p_atan(const float x)
+{
+    const float a1 =  0.9998660f;
+    const float a3 = -0.3302995f;
+    const float a5 =  0.1801410f;
+    const float a7 = -0.0851330f;
+    const float a9 =  0.0208351f;
+    float x2 = x * x;
+    return x * (a1 + x2 * (a3 + x2 * (a5 + x2 * (a7 + x2 * a9))));
+}
+
+
 /**
  *
  * Calculates the inverse tangent (arc tangent) of b/a. Stability not guaranteed
